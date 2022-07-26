61509a322d6f6280c444533b8ebaf10ecc632107
LEE JINYOUNG <swlsdud1005@naver.com>
2022-07-26 10:49:16 +0900

    home create

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const header = document.querySelector("#header");
+const headerHeight = header.getBoundingClientRect().height;
 const toggle = document.querySelector("#toggle");
 const navbar_menu = document.querySelector("#navbar_menu");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("navbar_dark");
+  } else {
+    header.classList.remove("navbar_dark");
+  }
+});
 
 document.addEventListener("click", function (e) {
   if (
