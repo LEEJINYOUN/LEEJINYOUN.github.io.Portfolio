43c2439895c1baf4e58289a33ab1984f2408310a
이진영 <swlsdud1005@naver.com>
2022-12-04 09:27:00 +0900

    변수명 수정

--- js/main.js
+++ js/main.js
@@ -3,31 +3,31 @@ const headerHeight = header.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
-    header.classList.add("navbar_dark");
+    header.classList.add("navDark");
   } else {
-    header.classList.remove("navbar_dark");
+    header.classList.remove("navDark");
   }
 });
 
-const toggle = document.querySelector("#toggle");
-const navbar_menu = document.querySelector("#navbar_menu");
+const navToggle = document.querySelector("#navToggle");
+const navMenu = document.querySelector("#navMenu");
 
 document.addEventListener("click", function (e) {
-  if (e.target.classList == "navbar_menu_item_link") {
-    toggle.classList.toggle("active");
-    navbar_menu.classList.toggle("active");
+  if (e.target.classList == "navMenuItemLink") {
+    navToggle.classList.toggle("active");
+    navMenu.classList.toggle("active");
   }
 });
 
-toggle.addEventListener("click", function () {
-  toggle.classList.toggle("active");
-  navbar_menu.classList.toggle("active");
+navToggle.addEventListener("click", function () {
+  navToggle.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
 const home = document.querySelector("#home");
 const homeHeight = home.getBoundingClientRect().height;
 
-const arrowUp = document.querySelector(".arrow-up");
+const arrowUp = document.querySelector(".arrowUp");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
     arrowUp.classList.add("visible");
